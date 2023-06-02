749ded8eae43208be9cb4e64efd673c489019fbe
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-06-02 20:06:35 +0530

    LoginPage and auth

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Signup from './Components/Signup';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Components/Login'
+import {LoginPage} from "./Components/LoginPage"
 function App() {
   return (
     <div className='App'>
       <Router>
         <Routes>
           <Route path="/" element={<Signup />} />
-          <Route path='/login' element={<Login/>} />
+          <Route path='/login' element={<LoginPage/>} />
         </Routes>
       </Router>
     </div>
