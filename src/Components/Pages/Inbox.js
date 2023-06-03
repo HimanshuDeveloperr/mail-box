f240ae4ddf96f80240acffacb33aee66b411e649
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-06-04 03:38:02 +0530

    mail optimization

--- src/Components/Pages/Inbox.js
+++ src/Components/Pages/Inbox.js
@@ -1,4 +1,5 @@
 
+
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
@@ -8,46 +9,54 @@ import { useDispatch, useSelector } from 'react-redux';
 import { ClassActions } from '../Store/ClassSlice';
 
 const Inbox = () => {
-   const deleteMail= useSelector(state=>state.auth.email)
-   console.log(deleteMail)
   const sentMail = localStorage.getItem('email').replace('.', '').replace('@', '');
 
   const [sentboxArr, setSentboxArr] = useState([]);
   const [loading, setIsLoading] = useState(false);
   const [mail, setMail] = useState(false);
+  const [unreadCount, setUnreadCount] = useState(0);
   const dispatch = useDispatch();
-  const classes = useSelector(state => state.class.class);
-//   const unreadCount = sentboxArr.filter(email => !classes).length;
-
-let unreadCount = 0;
-sentboxArr.forEach(email => {
-  if (!classes) {
-    unreadCount++;
-  }
-});
-
+  const classes = useSelector((state) => state.class.class);
 
   useEffect(() => {
-    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://mail-box--inbox-default-rtdb.firebaseio.com/${sentMail}.json`);
 
-    const FetchSentEmails = async () => {
-      const response = await axios.get(`https://mail-box--inbox-default-rtdb.firebaseio.com/${sentMail}.json`);
+        const data = response.data;
+        const updatedSentboxArr = [];
 
-      console.log(response);
+        for (const key in data) {
+          updatedSentboxArr.push({ id: key, ...data[key] });
+        }
 
-      const data = response.data;
-      const updatedSentboxArr = [];
-
-      for (const key in data) {
-        updatedSentboxArr.push({ id: key, ...data[key] });
+        setSentboxArr(updatedSentboxArr);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
       }
-
-      setSentboxArr(updatedSentboxArr);
-      setIsLoading(false);
     };
 
-    FetchSentEmails();
+    const interval = setInterval(() => {
+      fetchData();
+    }, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, [sentMail]);
+
+  useEffect(() => {
+    let count = 0;
+
+    sentboxArr.forEach((email) => {
+      if (!classes) {
+        count++;
+      }
+    });
+
+    setUnreadCount(count);
+  }, [sentboxArr, classes]);
 
   const mailHandler = () => {
     setMail(true);
@@ -61,19 +70,24 @@ sentboxArr.forEach(email => {
     dispatch(ClassActions.classHandler());
   };
 
-  const deleteHandler= async(id)=>{
-  
-      await axios.delete(`https://mail-box--inbox-default-rtdb.firebaseio.com/${sentMail}/${id}.json`)
+  const deleteHandler = async (id) => {
+    await axios.delete(`https://mail-box--inbox-default-rtdb.firebaseio.com/${sentMail}/${id}.json`);
 
-      const updatedSentboxArr = sentboxArr.filter(email => email.id !== id);
+    const updatedSentboxArr = sentboxArr.filter((email) => email.id !== id);
     setSentboxArr(updatedSentboxArr);
-  }
+
+    if (!classes) {
+      setUnreadCount((prevCount) => prevCount - 1);
+    }
+  };
 
   return (
     <div className="h-100 d-flex justify-content-center align-items-center">
       <Card className="w-75" style={{ background: 'transparent', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)', color: 'white' }}>
         <Card.Body>
-          <Card.Title style={{ color: '#555555' }} className='border-bottom'>InBox</Card.Title>
+          <Card.Title style={{ color: '#555555' }} className="border-bottom">
+            InBox
+          </Card.Title>
           {loading && <p>...loading</p>}
           {!loading && (
             <>
@@ -83,16 +97,20 @@ sentboxArr.forEach(email => {
                 sentboxArr.map((email) => (
                   <div key={email.id} className="mb-3 pb-3 border-bottom">
                     <p className="mt-2">
-                      {classes ? <span className='text-info'>--</span> : <span className='text-danger'>--</span>}
+                      {classes ? <span className="text-info">--</span> : <span className="text-danger">--</span>}
                       <strong>mail from:</strong> {email.from}
                     </p>
                     <p>
                       <strong>Subject:</strong> {email.subject}
                     </p>
                     <Link to={`/inboxmessages/${email.id}`} className="text-decoration-none">
-                      <Button variant="outline-info"  onClick={readHandler}>View Message</Button>
+                      <Button variant="outline-info" onClick={readHandler}>
+                        View Message
+                      </Button>
                     </Link>
-                    <Button variant='outline-danger' className='mx-2' onClick={() => deleteHandler(email.id)}>Delete</Button>
+                    <Button variant="outline-danger" className="mx-2" onClick={() => deleteHandler(email.id)}>
+                      Delete
+                    </Button>
                   </div>
                 ))
               )}
@@ -103,7 +121,7 @@ sentboxArr.forEach(email => {
         <Card.Footer>
           {!mail && <Button variant="outline-success" onClick={mailHandler}>Compose Mail</Button>}
           {mail && <ComposeMail />}
-          {mail && <Button variant='danger' className='m-2' onClick={cancelHandler}>Cancel</Button>}
+          {mail && <Button variant="danger" className="m-2" onClick={cancelHandler}>Cancel</Button>}
           <p className="text-muted text-white-50">Unread Emails: {unreadCount}</p>
         </Card.Footer>
       </Card>
@@ -112,3 +130,4 @@ sentboxArr.forEach(email => {
 };
 
 export default Inbox;
+
